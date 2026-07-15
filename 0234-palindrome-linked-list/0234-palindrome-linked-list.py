32c9b325ee9cfb43197622ff7d41639e1116b472
ayush beladiya <135732350+ayushjiii@users.noreply.github.com>
2026-07-15 16:43:32 +0530

    Time: 39 ms (40.63%), Space: 42.3 MB (94.07%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -7,25 +7,39 @@
 class Solution:
     def isPalindrome(self, head) -> bool:
         
-        fast  = head
-        slow = head
+        ## we will use total 3 algorithms here :
+        # 1. tortoise and heir (slow-fast pointer)
+        # 2. reverse LL 
+        # 3. Two pointers (left-right)
 
+
+        ## declaring the pointers
+        fast,slow = head,head
+
+        ## moving the pointers until fast reaches to the end
         while fast and fast.next :
             fast = fast.next.next
-            slow = slow.next
+            slow = slow.next       
+        
 
+        ## reverse the linked list from the middle
         prev = None
         while slow :
-            tmp = slow.next
+            tmp = slow.next 
             slow.next = prev
             prev = slow
             slow = tmp
 
+        ## checking if palindrome or not -> (True or False)
         left,right = head,prev
         while right :
-            if left.val != right.val:
+            if left.val != right.val :
                 return False
             left = left.next
             right = right.next
-        
+
         return True
+
+
+
+
